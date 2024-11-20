e877d1dd939a55ccfab4c2da98098011baa9194d
wen-3 <sd63324@gmail.com>
2024-11-20 19:44:13 +0800

    vue-loading-overlay

--- pages/person.vue
+++ pages/person.vue
@@ -1,6 +1,21 @@
+<script setup  >
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+
+const isLoading = ref(true);
+setTimeout(() => {
+    isLoading.value = false;
+}, 2000);
+
+</script>
+
 <template>
     <div>
         <h2>Person</h2>
         <img src="/image.png" alt="">
+        
+        <ClientOnly>
+            <Loading :active="isLoading"></Loading>
+        </ClientOnly>
     </div>
 </template>
